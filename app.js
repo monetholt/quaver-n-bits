8afbc477f0124714e7ca61240a3a7c5c42516616
monetholt <holtm@oregonstate.edu>
2020-08-02 18:50:01 -0700

    Add ability to edit instruments in profile

--- app.js
+++ app.js
@@ -387,11 +387,15 @@ app.get('/profile',checkAuthenticated,(req,res,next) => {
                 let context = {
                     user: req.user,
                     profile: rows[0][0],
-                    instruments: rows[1],
+                    profileInstruments: rows[1],
                     workSamples: rows[2]
                 };
-                console.log(context);
-                res.render('profile', context);
+
+                getInstrumentsAndLevels(req, res, context, complete);
+
+                function complete() {
+                    res.render('profile', context);
+                }
             } else {
                 throw(new ReferenceError("No profile found"));
             }
@@ -438,6 +442,63 @@ app.put('/profile/about', checkAuthenticated,(req, res, next) => {
             });
     } catch (err) {
         res.redirect(utils.profileUpdateErrorRedirect());
+    }
+});
+
+//updates the profile instruments. Called from within the profile view page.
+app.post('/profile/instruments', checkAuthenticated, (req, res, next) => {
+    try {
+        mysql.pool.getConnection(function (err, conn) {
+            if (err) throw (err);
+
+            var profileKey = req.body["id"];
+
+            //delete current instruments
+            conn.query(`DELETE FROM ProfileInstruments WHERE ProfileID = ? `, [profileKey],
+                function (err, rows) {
+
+                    if (err) {
+                        conn.release();
+                        res.write({ message: 'An error occurred when updating your instruments: '.JSON.stringify(err) });
+                        res.end();
+                    } else {
+
+                        //first format instrument/levelIDs sent in with form
+                        var instruments = [];
+
+                        var timestamp = new Date().toISOString().slice(0, 19).replace('T', ' '); //timestamp for create/lastupdated
+                        for (i = 0; i <= 20; i++) { //set arbitary max of 20 instruments for now
+
+                            if (Object.prototype.hasOwnProperty.call(req.body, "instruments[" + i + "][InstrumentKey]") &&
+                                req.body["instruments[" + i + "][InstrumentKey]"] > 0) {
+
+                                instruments.push([
+                                    req.body["instruments[" + i + "][InstrumentKey]"],
+                                    req.body["instruments[" + i + "][LevelKey]"],
+                                    profileKey, timestamp, timestamp]);
+
+                            }
+                            else break;
+                        }
+
+                        ////add the instruments
+                        conn.query(`INSERT INTO ProfileInstruments (InstrumentID, LevelID, ProfileID, CreateDate, LastUpdated)  VALUES ?  `, [instruments],
+                            function (err, rows) {
+                                conn.release();
+
+                                if (err) {
+                                    res.write({ message: 'An error occurred when updating your instruments: '.JSON.stringify(err) });
+                                    res.end();
+                                } else res.send({ success: 1, message: 'Successfully updated instruments.' });
+
+                            });
+                    }
+                });
+
+        });
+    } catch (err) {
+        res.write(JSON.stringify(err));
+        res.end();
     }
 });
 
--- static/js/profile.js
+++ static/js/profile.js
@@ -158,3 +158,177 @@ window.addEventListener('DOMContentLoaded', () => {
     req.send(null);
 });
 
+
+//returns a list of the instruments in the edit form of the passed id
+function getEditInstruments() {
+    //Go to the edit form for this add and grab all instruments
+    let adInstruments = $("#instrument-list").find("li");
+
+    let instrumentList = [];
+
+    //add instruments to array
+    adInstruments.each(function () {
+        let instID = $(this).find(".inst-list-inst").attr("data-val");
+        let levelID = $(this).find(".inst-list-level").attr("data-val");
+        let instName = $(this).find(".inst-list-inst").text();
+        let levelName = $(this).find(".inst-list-level").text();
+
+        instrumentList.push({
+            "InstrumentKey": instID,
+            "LevelKey": levelID,
+            "Instrument": instName,
+            "Level": levelName
+        });
+    });
+
+    //return list of instruments
+    return instrumentList;
+}
+
+//populates edit instrument form with current instruments
+function populateInstrumentsEditForm() {
+
+    let instruments = getEditInstruments(); //get instrument data from instrument section of profile
+
+    $.each(instruments, function (i, instrument) {
+        var sectionID = addSelection(instrument.InstrumentKey, instrument.Instrument, "edit"); //add this instrument to the form
+        $("#" + sectionID).find("#level-" + sectionID).val(instrument.LevelKey); //set the level
+    });
+}
+
+//when passed an instrument id + name, will add a new section under the instrument select so the user can select level or delete.
+//will show user an error if they try to add the same instrument twice
+function addSelection(id, inst) {
+    var selectionDiv = $("#instrument-selection-edit"); //grab section below instrument select
+
+    var thisId = id + "-" + Date.now();
+
+    selectionDiv.append("<div class='instrument-selection-item' id='" + thisId + "'></div>"); //add a new 'box' for this selected instrument
+
+    var thisSelection = selectionDiv.find("#" + thisId);
+
+    thisSelection.append("<div class='delete-selection'>X</div>"); //add the icon to delete this section
+    var levelSelect = $("#level-list-main-edit").clone(); //copy the existing level selector so this instrument can have its own
+    levelSelect.attr("id", "level-" + thisId); //set the id for the level select + unhide it
+    levelSelect.css("display", "inline-block");
+
+    thisSelection.append("<div class='instrument-selection-text'>" + inst + "</div>");
+    thisSelection.append(levelSelect);
+
+    return thisId;
+}
+
+
+//returns html for instrument list for instrument section when passed in list of instruments/levels.
+function createInstrumentList(instruments) {
+    let list = ``;
+    for (let i in instruments) {
+        list += `<li><span class='inst-list-inst' data-val='${instruments[i]['InstrumentKey']}'>${instruments[i]['Instrument']}</span>
+            <span class='inst-list-level' data-val='${instruments[i]['LevelKey']}'>(${instruments[i]['Level']})</span></li>`;
+    }
+    return list;
+}
+
+
+$(document).ready(function () {
+
+    //set up the instrument to use select2 for easy searching+selecting. This seemed closest to the datalist we were using
+    $('#instrument-list-edit').select2({
+        placeholder: "Select an instrument"
+    }).on('select2:select', function (e) {
+        //whenever we select a new instrument, add it to the section below to select level
+        var selectedOption = e.params.data;
+        addSelection(selectedOption.id, (selectedOption.text), $(this).attr("id").split("-")[2]);
+    });
+
+    //click event to remove selected instruments.
+    $("body").on("click", ".delete-selection", function () {
+        $(this).parent().remove();
+    });
+
+    $("#edit-instruments").click(function () {
+        $("#instrument-selection-edit").html(""); //clear out any selected instruments
+        $("#error-msg-edit").html(""); //clear out any error msg'
+        populateInstrumentsEditForm(); //populate form with this ad info
+
+        $("#modal-edit-instruments").foundation('open'); //show form to user
+    });
+
+    //add event on instrument save to put instruments back into main form.
+    $("#instrument-section-edit-btn").click(function () {
+
+        var id = $(this).attr("data-id"); //profileID
+
+        $("#error-msg-edit").html(""); //clear out any error msg
+
+        var selectedInstruments = $("#modal-edit-instruments").find(".instrument-selection-item"); //get all selected instruments
+
+        //make sure the user chose at least one
+        if (selectedInstruments.length < 1) {
+            $("#error-msg-edit").html("Please select at least one instrument");
+        }
+        else {
+            //gather IDs + make sure levels are all selected
+            var errorMsg = ""; //this will be non-blank if we find an error
+            var selected = [];
+            var idlevelcombos = []; //instrument/level combo must be unique so create an array to check
+
+            selectedInstruments.each(function () {
+                var instID = $(this).attr("id").split("-")[0]; //get the instrument ID
+                var levelID = $(this).find("select.selection-level").val(); //get the levelID
+                var instrumentName = $(this).find(".instrument-selection-text").text();
+                var levelName = $(this).find("select.selection-level").find("option:selected").text();
+
+                if (levelID < 1) { //this instrument doesnt have a level selected
+                    errorMsg = "Please select an experience level for each instrument";
+                    return false;
+                }
+                else if ($.inArray(instID + "-" + levelID, idlevelcombos) !== -1) {
+                    errorMsg = "Hey, if you select more than one of the same instrument you need to select different levels for each";
+                    return false;
+                }
+                else {
+                    selected.push({
+                        "InstrumentKey": instID,
+                        "LevelKey": levelID,
+                        "Instrument": instrumentName,
+                        "Level": levelName
+                    }); //add the ids to our data array
+
+                    idlevelcombos.push(instID + "-" + levelID); //add the ids combo to our lookup
+                }
+            });
+
+            if (errorMsg != "") { //if we found an error, show it
+                $("#error-msg-edit").html(errorMsg);
+            }
+            else {
+
+                //if no errors, go ahead and send to server to save.
+                $.ajax({
+                    type: "POST",
+                    url: "/profile/instruments",
+                    data: {
+                        "id": id,
+                        "instruments": selected,
+                    },
+                    success: function (data, textStatus, jqXHR) {
+                        $("#modal-edit-instruments").foundation('close'); //close popup
+
+                        //data - response from server
+                        if (data.success == 1) {
+                            $("#instrument-list").html(createInstrumentList(selected)); //update instrument list on profile
+                            $("#modal-edit-instruments").foundation('close'); //close popup
+                        }
+                        else $("#error-msg-edit").html("Error saving instruments."); //error! show it on the form
+                       
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        $("#error-msg-edit").html("Error saving instruments.");
+                    },
+                    dataType: 'json'
+                });
+            }
+        }
+    });
+});

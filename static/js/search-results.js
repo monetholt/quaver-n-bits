9348dae2d8b19d56f9f35646590535298ef0171c
natetonal <nsktrombone84@hotmail.com>
2020-08-10 01:07:18 -0700

    Added ZipCodeAPI functionality so results are filtered out by radius.

--- static/js/search-results.js
+++ static/js/search-results.js
@@ -1,8 +1,58 @@
  document.addEventListener('DOMContentLoaded', (event) => {
 
+
+     filterByZipCode();
      randomBG();
 
+     // Deletes any children in the DOM that are outside of the given zip code radius.
+     function filterByZipCode() {
 
+         // Check for origin. If it doesn't exist, no need to do anything.
+         if (!document.getElementById('adzipcode')) { return; }
+
+         // Get the origin zip and radius.
+         let origin = document.getElementById('adzipcode').value;
+         let radius = document.getElementById('adlocrad').value;
+
+         // Only perform search if radius is greater than 0, since 0 indicates the ad searches any location.
+         if (radius > 0) {
+
+             // API Key (we might want to move this).
+             const key = 'js-tdNwFATA32aht796TZoLdODFiay74Fs5Kj4eTsOobne4ZWjs8uDmItN8K1GQKOph';
+
+             // Open request to ZipCodeAPI to get all zips that are within the given radius of this origin.
+             let req = new XMLHttpRequest();
+             req.open('GET', `https://www.zipcodeapi.com/rest/${key}/radius.json/${origin}/${radius}/mile?minimal`, true);
+             req.addEventListener('load', () => {
+                 if (req.status < 400) {
+                     // Get the response as an array of zip codes.
+                     let zips = JSON.parse(req.responseText)["zip_codes"];
+
+                     // Get the parent holding the search results and all of its child nodes.
+                     let parent = document.getElementById('search-results');
+                     let children = parent.children;
+
+                     // Iterate through the children. If the child's zip is not in the radius zips, remove it.
+                     Object.values(children).forEach(child => {
+                         if (!zips.includes(child.getAttribute('data-zip'))) {
+                             parent.removeChild(child);
+                         }
+                     });
+
+                     // If all of the children got removed after checking the radius, display the "no matches" message.
+                     if (parent.children.length == 0) {
+                         document.getElementById('no-matches').classList.remove('hidden');
+                         document.getElementById('search-results-container').classList.add('hidden');
+                     }
+                 } else {
+                     console.log("Something bad happened." + req.statusText);
+                 }
+             });
+
+             // We're limited on requests, so comment this out to test non zip code related things.
+             req.send(null);
+         }
+     }
      function randomBG() {
          let header = document.getElementsByClassName('search-result-header');
          Object.values(header).forEach(result => {

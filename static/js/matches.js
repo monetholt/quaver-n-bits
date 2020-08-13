84fe2cc26442a0e6ec78b6b7b5ceded7e4b57cf7
monetholt <holtm@oregonstate.edu>
2020-08-12 20:31:00 -0700

    Added ajax calls for match button actions + Fixed unrelated typo in dashboard

--- static/js/matches.js
+++ static/js/matches.js
@@ -3,3 +3,119 @@ window.addEventListener('DOMContentLoaded', () => {
         // TODO: Go to page.
     }
 });
+
+
+//TODO
+//removes match from the UI after a reject/block
+function removeMatchUI(id) {
+
+}
+
+
+//TODO
+//Adds match to the connections section after a match accept.
+function addMatch2Connections(id) {
+
+}
+
+//accepts match w/passedID
+function acceptMatch(id) {
+    $.ajax({
+        type: "POST",
+        url: "/matches/accept/" + id,
+        data: {},
+        success: function (success, textStatus, jqXHR) {
+            //data - response from server
+            if (success && success.success > 0) { //successfull update
+
+                showAlert("success", "far fa-check-circle", "You are now connected!");
+                addMatch2Connections(id);
+
+            }
+            else {
+                showAlert("warning", "fa fa-exclamation-triangle", "Error accepting match request."); //something went wrong
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            showAlert("warning", "fa fa-exclamation-triangle", "Error accepting match request."); //something went wrong
+        },
+        dataType: 'json'
+    });
+}
+
+//rejects match w/passedID
+function rejectMatch(id) {
+    $.ajax({
+        type: "POST",
+        url: "/matches/reject/" + id,
+        data: {},
+        success: function (success, textStatus, jqXHR) {
+            //data - response from server
+            if (success) { //successfull update
+
+                showAlert("success", "far fa-check-circle", "Match request successfully rejected.");
+                removeMatchUI(id); //remove Match from UI
+
+            }
+            else {
+                showAlert("warning", "fa fa-exclamation-triangle", "Error rejecting match request."); //something went wrong
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            showAlert("warning", "fa fa-exclamation-triangle", "Error rejecting match request."); //something went wrong
+        },
+        dataType: 'json'
+    });
+}
+
+
+//blocks match w/passedID
+function blockMatch(id) {
+    $.ajax({
+        type: "POST",
+        url: "/matches/reject/" + id,
+        data: {},
+        success: function (success, textStatus, jqXHR) {
+            //data - response from server
+            if (success) { //successfull update
+
+                showAlert("success", "far fa-check-circle", "User successfully blocked.");
+                removeMatchUI(id); //remove Match from UI
+
+            }
+            else {
+                showAlert("warning", "fa fa-exclamation-triangle", "Error blocking connection."); //something went wrong
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            showAlert("warning", "fa fa-exclamation-triangle", "Error blocking connection."); //something went wrong
+        },
+        dataType: 'json'
+    });
+}
+
+
+//disconnects match w/passedID
+function disconnectMatch(id) {
+    $.ajax({
+        type: "POST",
+        url: "/matches/disconnect/" + id,
+        data: {},
+        success: function (success, textStatus, jqXHR) {
+            //data - response from server
+            if (success && success.success > 0) { //successfull update
+
+                showAlert("success", "far fa-check-circle", "Connection successfully removed.");
+                removeMatchUI(id); //remove Match from UI
+
+            }
+            else {
+                showAlert("warning", "fa fa-exclamation-triangle", "Error disconnecting match."); //something went wrong
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            showAlert("warning", "fa fa-exclamation-triangle", "Error disconnecting match."); //something went wrong
+        },
+        dataType: 'json'
+    });
+}
